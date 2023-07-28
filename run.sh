08dedd903a9ff9cd672aaa4c2c39706bb99d190e
Sid Dange <dange.siddhant@gmail.com>
2023-07-28 14:29:12 -0400

    update release url

--- run.sh
+++ run.sh
@@ -71,7 +71,7 @@ case "$RUNNER_OS" in
 esac
 
 BUILDPULSE_TEST_REPORTER_HOSTS=(
-	https://github.com/buildpulse/test-reporter/releases/download/untagged-fc2bb11297dbaf607811
+	https://github.com/buildpulse/test-reporter/releases/download/v0.26.1
 )
 [ -n "${INPUT_CLI_HOST}" ] && BUILDPULSE_TEST_REPORTER_HOSTS=("${INPUT_CLI_HOST}" "${BUILDPULSE_TEST_REPORTER_HOSTS[@]}")
 
